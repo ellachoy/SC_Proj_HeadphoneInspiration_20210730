2084362290699e4547ec06c73f4bd8f629936198
Ella Choy <egal21de@gmail.com>
2021-08-02 00:51:58 +0200

    newsletter

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+console.log("hallo Ella");
+
+function showFormValues(){
+    console.log(document.getElementById("firstname").value);
+  console.log(document.getElementById("laststname").value);
+  console.log(document.getElementById("email").value);
+
+//   result.innerHTML =
+}
